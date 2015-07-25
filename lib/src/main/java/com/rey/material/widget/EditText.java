223c30353c6c3a0934c4acb83bb19104e5489d3a
Rey Pham <pea5137@gmail.com>
2015-07-26 01:38:41 +0700

    EditText: fix bug measure view with null layout params.

--- lib/src/main/java/com/rey/material/widget/EditText.java
+++ lib/src/main/java/com/rey/material/widget/EditText.java
@@ -514,7 +514,7 @@ public class EditText extends FrameLayout implements ThemeManager.OnThemeChanged
 		int supportWidth = 0;
 		int supportHeight = 0;
 		
-		if(mLabelView != null){
+		if(mLabelView != null && mLabelView.getLayoutParams() != null){
 			mLabelView.measure(tempWidthSpec, tempHeightSpec);
 			labelWidth = mLabelView.getMeasuredWidth();
 			labelHeight = mLabelView.getMeasuredHeight();
@@ -524,7 +524,7 @@ public class EditText extends FrameLayout implements ThemeManager.OnThemeChanged
 		inputWidth = mInputView.getMeasuredWidth();
 		inputHeight = mInputView.getMeasuredHeight();
 		
-		if(mSupportView != null){
+		if(mSupportView != null && mSupportView.getLayoutParams() != null){
 			mSupportView.measure(tempWidthSpec, tempHeightSpec);
 			supportWidth = mSupportView.getMeasuredWidth();
 			supportHeight = mSupportView.getMeasuredHeight();
@@ -560,12 +560,12 @@ public class EditText extends FrameLayout implements ThemeManager.OnThemeChanged
 		setMeasuredDimension(width, height);		
 		
 		tempWidthSpec = MeasureSpec.makeMeasureSpec(width - getPaddingLeft() - getPaddingRight(),  MeasureSpec.EXACTLY);
-		if(mLabelView != null)
+		if(mLabelView != null && mLabelView.getLayoutParams() != null)
 			mLabelView.measure(tempWidthSpec, tempHeightSpec);
 		
 		mInputView.measure(tempWidthSpec, tempHeightSpec);
 		
-		if(mSupportView != null)
+		if(mSupportView != null && mSupportView.getLayoutParams() != null)
 			mSupportView.measure(tempWidthSpec, tempHeightSpec);				
 	}	
 
